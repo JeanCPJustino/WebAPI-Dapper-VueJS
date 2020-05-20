0153450d291baf6da685d9e67b2ae00125f34703
JeanCPJustino <jeancarlospereirajustino@gmail.com>
2020-05-19 22:35:18 -0300

    Primeiro Commit do Projeto

---
+++ frontend/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <Alunos />
+  </div>
+</template>
+
+<script>
+import Alunos from "./components/Aluno/Alunos.vue";
+
+export default {
+  name: "App",
+  components: {
+    Alunos,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+
+body {
+  background-color: #eee;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  justify-items: center;
+}
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
+
+#app {
+}
+
+.btn_red {
+  background-color: rgb(236, 51, 88);
+}
+
+.btn {
+  padding: 10px 20px;
+  cursor: pointer;
+  border: 1px solid #000;
+  color: white;
+  font-weight: bold;
+  /* Para as bordas do botão ficarem arredondadas */
+  border-radius: 5px;
+  /* Para aplicar um efeito na borda inferior do botão */
+  border-bottom: 3px solid black;
+}
+
+/*Aplica um efeito visual sobre o botão quando passamos o cursor sobre o mesmo*/
+.btn:hover {
+  text-shadow: 1px 1px 1px black;
+  margin-top: 3px;
+  border-bottom: 1px solid black;
+}
+
+table {
+  margin: 0px;
+  padding: 0px;
+  list-style-type: none;
+  width: 100%;
+}
+
+table thead th{
+  background-color: rgb(184, 208, 216) !important;
+  font-size: 1.2em;
+  padding: 10px 0px;
+  text-align: center !important;
+}
+
+table tr td {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+
+.colPequeno {
+  width: 5%;
+}
+</style>
---
+++ frontend/src/components/Aluno/Alunos.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <titulo texto="Aluno" />
+    <div>
+      <input
+        type="text"
+        placeholder="Nome do Aluno"
+        v-model="nome"
+        @keyup.enter="addAluno()"
+      />
+      <button class="btn btn_add" @click="addAluno()">Adicionar</button>
+    </div>
+    <br />
+    <br />
+    <table>
+      <thead>
+        <th>Mat.</th>
+        <th>Nome</th>
+        <th>Opções</th>
+      </thead>
+      <tbody v-if="alunos.length">
+        <tr v-for="(aluno, index) in alunos" :key="index">
+          <td>{{ aluno.id }}</td>
+          <td>{{ aluno.nome }}</td>
+          <td>
+            <button class="btn btn_red" @click="remover(aluno)">Remover</button>
+          </td>
+        </tr>
+      </tbody>
+      <tfoot v-else>
+        Nenhum aluno cadastrado
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import Titulo from "../_share/Titulo";
+
+export default {
+  components: {
+    Titulo,
+  },
+  data() {
+    return {
+      titulo: "Aluno",
+      nome: "",
+      alunos: [],
+    };
+  },
+
+  created() {
+    /* O $http executa um GET e o retorno deste GET é passado para o ret, que 
+       consequentemente possui é uma STRING e deve ser convertida para JSON. Logo,
+       este novo resultado já convertido é atribuído para a variável alunos, que por
+       sua vez é passado para outro objeto no último "this"*/
+    this.$http
+      .get("http://localhost:5000/api")
+      .then((ret) => ret.json())
+      .then((alunos) => (this.alunos = alunos));
+  },
+
+  props: {},
+  methods: {
+    addAluno() {
+      let _aluno = {
+        nome: this.nome,
+        sobrenome: "",
+      };
+
+      this.$http
+        .post("http://localhost:5000/api", _aluno)
+        .then((ret) => ret.json())
+        .then((alunoinserido) => {
+          this.alunos.push(alunoinserido);
+          this.nome = "";
+        });
+    },
+    remover(aluno) {
+      this.$http.delete(`http://localhost:5000/api/${aluno.id}`).then(() => {
+        let index = this.alunos.indexOf(aluno);
+        this.alunos.splice(index);
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+input {
+  /*Calculo realizado para que o botão fique na frente do Input, caso o "display: inline não for utilizado"*/
+  /* width: calc(100%-150px); */
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  color: #9ea3a3;
+  display: inline;
+}
+
+.btn_add {
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  display: inline;
+  background-color: rgb(116, 115, 115);
+}
+
+.btn_add:hover {
+  padding: 20px;
+  margin: 0px;
+  border: 0px;
+}
+</style>
